65c1dca13cf9d185391a9ceccbfbf926a028953b
Red Ochsenbein <red@redochsenbein.ch>
2026-05-16 19:16:35 +0200

    chore: fix build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 android {
     namespace = "com.example.voidweaver"
     compileSdk = flutter.compileSdkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
