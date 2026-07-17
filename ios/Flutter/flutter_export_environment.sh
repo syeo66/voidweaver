6ac4db3979c9563c98760f378e1e91389aedd87e
Red Ochsenbein <red@redochsenbein.ch>
2026-07-17 21:54:19 +0200

    chore: update configs

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/redochsenbein/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/redochsenbein/private/voidweaver"
+export "FLUTTER_ROOT=/Users/red/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/red/private/voidweaver"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/red/private/voidweaver/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
